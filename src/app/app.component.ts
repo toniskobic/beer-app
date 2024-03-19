b6d8aefe09643c4610a6157b177a93dbfd444b31
tskobic <toni.skobic@gmail.com>
2024-03-19 23:56:36 +0100

    feat: load beers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
+import { HeaderComponent } from './components/header/header.component';
+import { BeerListComponent } from './components/beer-list/beer-list.component';
+import { Beer } from './models/beer.model';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [RouterOutlet, HeaderComponent]
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, BeerListComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  beers = signal<Beer[]>([]);
+
+  ngOnInit(): void {
+    const favourites = JSON.parse(sessionStorage.getItem('favourites') || '[]');
+    const beers: Beer[] = [
+      {
+        id: 1,
+        name: 'Beer 1',
+        description: 'This is the first beer',
+        abv: 5.5,
+        image_url: 'https://images.punkapi.com/v2/keg.png',
+      },
+      {
+        id: 2,
+        name: 'Beer 2',
+        description: 'This is the second beer',
+        abv: 6.0,
+        image_url: 'https://images.punkapi.com/v2/keg.png',
+      },
+      {
+        id: 3,
+        name: 'Beer 3',
+        description: 'This is the third beer',
+        abv: 6.5,
+        image_url: 'https://images.punkapi.com/v2/keg.png',
+      },
+    ];
+
+    beers.forEach((beer) => {
+      beer.favourite = favourites.includes(beer.id);
+    });
+
+    this.beers.set(beers);
+  }
+}
--- src/app/components/beer-list/beer-list.component.ts
+++ src/app/components/beer-list/beer-list.component.ts
@@ -1,12 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, model } from '@angular/core';
+import { Beer } from '../../models/beer.model';
+import { BeerCardComponent } from '../beer-card/beer-card.component';
 
 @Component({
   selector: 'app-beer-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [BeerCardComponent],
   templateUrl: './beer-list.component.html',
   styleUrl: './beer-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BeerListComponent {}
+export class BeerListComponent {
+  beers = model.required<Beer[]>();
+
+  onFavouriteChange(id: number, favourite: boolean) {
+    const beers = this.beers().map((beer) =>
+      beer.id === id ? { ...beer, favourite } : beer
+    );
+    this.beers.set(beers);
+  }
+}
