a27d09d9aef22c13520a2a9ec2a6807ab6664e02
tskobic <toni.skobic@gmail.com>
2024-03-20 18:44:22 +0100

    refactor: use dialog service

--- src/app/components/beer-card/beer-card.component.ts
+++ src/app/components/beer-card/beer-card.component.ts
@@ -4,35 +4,38 @@ import {
   Component,
   input,
   output,
-  signal,
 } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { Beer } from '../../models/beer.model';
 import { RippleModule } from 'primeng/ripple';
 import { SessionStorageKeys } from '../../constants/constants';
-import { DialogModule } from 'primeng/dialog';
+import { DialogService } from 'primeng/dynamicdialog';
 import { BeerDialogComponent } from '../beer-dialog/beer-dialog.component';
 
 @Component({
   selector: 'app-beer-card',
   standalone: true,
+  imports: [ButtonModule, CommonModule, RippleModule, BeerDialogComponent],
+  providers: [DialogService],
   templateUrl: './beer-card.component.html',
   styleUrl: './beer-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    ButtonModule,
-    CommonModule,
-    RippleModule,
-    DialogModule,
-    BeerDialogComponent,
-  ],
 })
 export class BeerCardComponent {
   beer = input.required<Beer>();
 
   favouriteChange = output<boolean>();
 
-  isDialogVisible = signal(false);
+  constructor(public dialogService: DialogService) {}
+
+  showDetails() {
+    this.dialogService.open(BeerDialogComponent, {
+      modal: true,
+      styleClass: 'dialog',
+      header: this.beer().name,
+      data: { beer: this.beer() },
+    });
+  }
 
   toggleFavourite() {
     const { id, favourite } = this.beer();
@@ -50,9 +53,5 @@ export class BeerCardComponent {
     );
 
     this.favouriteChange.emit(!favourite);
-  }
-
-  dialogVisibleChange(visible: boolean) {
-    this.isDialogVisible.set(visible);
   }
 }
--- src/app/components/beer-dialog/beer-dialog.component.ts
+++ src/app/components/beer-dialog/beer-dialog.component.ts
@@ -1,23 +1,20 @@
 import { CommonModule } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  input,
-  model,
-} from '@angular/core';
-import { DialogModule } from 'primeng/dialog';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { Beer } from '../../models/beer.model';
+import { DynamicDialogConfig } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-beer-dialog',
   standalone: true,
-  imports: [CommonModule, DialogModule],
+  imports: [CommonModule],
   templateUrl: './beer-dialog.component.html',
   styleUrl: './beer-dialog.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BeerDialogComponent {
-  beer = input.required<Beer>();
+  beer = signal<Beer | undefined>(undefined);
 
-  visible = model.required<boolean>();
+  constructor(public config: DynamicDialogConfig) {
+    this.beer.set(this.config.data.beer as Beer);
+  }
 }
